dc02b95c93fbfb3cf0d8fd6df1678265929d5fe4
xxs1993 <xiao.x@husky.neu.edu>
2018-11-06 10:53:05 -0500

    a

--- src/main/java/edu/neu/coe/info6205/sort/simple/SelectionSort.java
+++ src/main/java/edu/neu/coe/info6205/sort/simple/SelectionSort.java
@@ -1,12 +1,33 @@
 package edu.neu.coe.info6205.sort.simple;
 
-import static edu.neu.coe.info6205.sort.simple.Helper.less;
-import static edu.neu.coe.info6205.sort.simple.Helper.swap;
-
 public class SelectionSort<X extends Comparable<X>> implements Sort<X> {
+
+    /**
+     * Constructor for SelectionSort
+     *
+     * @param helper an explicit instance of Helper to be used.
+     */
+    public SelectionSort(Helper<X> helper) {
+        this.helper = helper;
+    }
+
+    public SelectionSort() {
+        this(new Helper<>("SelectionSort"));
+    }
 
     @Override
     public void sort(X[] xs, int from, int to) {
         // TODO implement selection sort
     }
+
+    @Override
+    public String toString() {
+        return helper.toString();
+    }
+
+    public Helper<X> getHelper() {
+        return helper;
+    }
+
+    private final Helper<X> helper;
 }
